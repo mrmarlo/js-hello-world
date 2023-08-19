29dc96513739ffcb37074c4c58469997bea31032
mrmarlo <kevinmarlo@netscape.net>
2023-08-19 00:11:25 -0500

    Hello World changed 08-18-2023

--- hello.js
+++ hello.js
@@ -4,19 +4,22 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("I changed the message to What's Up!");
+console.log(1);
+console.log("Message 1, " + "Message 2");
+console.log("Trees are very heavy ");
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
+   2. Figure out what the parentheses do. Will the code work without them? Syntax Error thrown!
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")? Syntax Error thrown in each scinario!
+   4. Remove the semi-colon, ;. It will still run. I believe it's because there's only one line of code. 
    5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   6. Print multiple messages one after the other. ?
+   7. Print two messages on the same line. Message 1, Message 2 printed! 
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". I couldn't get the qoutes to print!
    9. Other. You choose!
 */
